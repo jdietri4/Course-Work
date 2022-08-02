94b1043a7c37e7832f804c94497e86b01c6d8ff8
Jacob Dietrich <48534464+jdietri4@users.noreply.github.com>
2022-08-02 17:48:03 -0400

    Delete Lab1_Exercise.py

--- Lab1_Exercise.py
+++
@@ -1,9 +0,0 @@
-def main():
-    celsius_temps = [23.4, 10.2, 8.9, 15.8, 24.3, 0.8]
-    celsius_temps = sorted(celsius_temps, reverse=True)
-    valueToConvert = celsius_temps[1]
-    valueInF = (valueToConvert*1.8) + 32
-    print(valueInF)
-
-if __name__== "__main__":
-    main()
